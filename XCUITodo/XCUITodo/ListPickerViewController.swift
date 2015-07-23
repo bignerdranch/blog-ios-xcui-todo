65299105dc0fda6c3c6e105f7b62b191f39c253b
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2015-07-23 13:28:41 -0400

    removes dead code formerly called by Add button

--- XCUITodo/XCUITodo/ListPickerViewController.swift
+++ XCUITodo/XCUITodo/ListPickerViewController.swift
@@ -33,12 +33,6 @@ class ListPickerViewController: UITableViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    func insertNewObject(sender: AnyObject) {
-        objects.insert(NSDate(), atIndex: 0)
-        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-    }
-
     // MARK: - Segues
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
