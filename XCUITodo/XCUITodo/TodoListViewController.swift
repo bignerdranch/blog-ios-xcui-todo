818d656bc7f58af5a7cb9639760e7563f3d1bcc6
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2015-07-23 17:43:38 -0400

    extracts computed property |filteredTodos|

--- XCUITodo/XCUITodo/TodoListViewController.swift
+++ XCUITodo/XCUITodo/TodoListViewController.swift
@@ -12,11 +12,14 @@ class TodoListViewController: UITableViewController {
 
     private var todos: [Todo] = []
     private var filter: (Todo -> Bool)!
+    private var filteredTodos: [Todo] {
+        return todos.filter(filter)
+    }
 
     func configure(todos todos: [Todo], filter: Todo -> Bool) {
         self.todos = todos
         self.filter = filter
-        print("\(self.title): configured with \(todos.filter(filter).count) todos")
+        print("\(self.title): configured with \(filteredTodos.count) todos")
     }
 
     var empty: Bool {
