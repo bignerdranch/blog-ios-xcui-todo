969275ef9fc45dd4566d0545be24c32800fddefb
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2015-07-23 15:15:08 -0400

    adds Todo type

---
+++ XCUITodo/XCUITodo/Todo.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  XCUITodo
+//
+//  Created by Jeremy on 2015-07-23.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import Foundation
+
+struct Todo {
+    let title: String
+    let due: NSDate
+    let finished: Bool
+}
