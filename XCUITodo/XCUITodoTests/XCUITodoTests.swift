21dd4eb742d9982cac750b0645e96dc39ec80c59
David Lam <david@buddybuild.com>
2016-09-29 16:16:04 -0700

    Break tests

--- XCUITodo/XCUITodoTests/XCUITodoTests.swift
+++ XCUITodo/XCUITodoTests/XCUITodoTests.swift
@@ -24,6 +24,7 @@ class XCUITodoTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertFalse(true);
     }
 
     func testPerformanceExample() {
